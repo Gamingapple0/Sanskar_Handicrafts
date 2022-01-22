4f08c256906e70af399757c3a1be0eea1502aeb0
Admin <corbettjewelry991.com>
2022-01-22 17:28:07 +0545

    PostgresDB setup and static root added

--- sanskar_handicrafts/settings.py
+++ sanskar_handicrafts/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'sanskar_handicrafts.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db',
+        'USER': 'apple',
+        'PASSWORD': 'Apple98130',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -117,10 +121,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static'),
 ]
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
